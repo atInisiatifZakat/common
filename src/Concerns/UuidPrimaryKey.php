e3546c35d3c933f3b4ffbd47f27fb54c701903af
kaizen-ci <info@kaizen-ci.org>
2021-01-20 09:00:08 +0000

    Common - Add branch
    Common - Add branch

---
+++ src/Concerns/UuidPrimaryKey.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Inisiatif\Package\Common\Concerns;
+
+use Ramsey\Uuid\Uuid;
+use Illuminate\Database\Eloquent\Model;
+
+trait UuidPrimaryKey
+{
+    public function getIncrementing(): bool
+    {
+        return false;
+    }
+
+    public function getKeyType(): string
+    {
+        return 'string';
+    }
+
+    public static function bootUuidPrimaryKey(): void
+    {
+        static::creating(function (Model $model): void {
+            if (!isset($model->attributes[$model->getKeyName()])) {
+                $model->incrementing = false;
+
+                $uuid = Uuid::uuid6();
+
+                $model->attributes[$model->getKeyName()] = $uuid->toString();
+            }
+        });
+    }
+}
