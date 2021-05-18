5bce4bc6427708a50daa21273b6e57295098bf46
kaizen-ci <info@kaizen-ci.org>
2021-05-18 18:44:45 +0000

    Fixing version

---
+++ src/Exceptions/DomainException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inisiatif\Package\Common\Exceptions;
+
+abstract class DomainException extends \Exception
+{
+}
---
+++ src/Exceptions/DomainModelExistException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inisiatif\Package\Common\Exceptions;
+
+final class DomainModelExistException extends DomainException
+{
+}
