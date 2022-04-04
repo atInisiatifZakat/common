fd90ff6a4dd8246cd6e642aad0639bbc367c233b
kaizen-ci <info@kaizen-ci.org>
2022-04-04 04:03:23 +0000

    Merge pull request #235 from atInisiatifZakat/develop
    Fixing testing

---
+++ src/DataTransfers/Caster/DateTimeCaster.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inisiatif\Package\Common\DataTransfers\Caster;
+
+use DateTimeInterface;
+use Illuminate\Support\Carbon;
+use Spatie\DataTransferObject\Caster;
+
+final class DatetimeCaster implements Caster
+{
+    public function cast(mixed $value): DateTimeInterface
+    {
+        return Carbon::parse($value);
+    }
+}
